3e04d4278186a32a72e4a719acd5252812cf8691
tina513 <dlnaz.ai@hotmail.com>
2016-05-31 15:52:11 -0400

    initial commit from api repo

---
+++ scripts/change-password-json.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#curl "http://localhost:3000/change-password/${ID}" \
+curl --include --request PATCH http://localhost:3000/change-password/$ID \
+  --header "Authorization: Token token=$TOKEN" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "an example password",
+      "new": "super sekrit"
+    }
+  }'
+
+# data output from curl doesn't have a trailing newline
+echo
---
+++ scripts/sign-up-json.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl "http://localhost:3000/sign-up" \
+ --include \
+ --request POST \
+ --header "Content-Type: application/json" \
+ --data "{
+   \"credentials\": {
+     \"email\": \"$EMAIL\",
+     \"password\": \"$PASSWORD\",
+     \"password_confirmation\": \"$PASSWORD\"
+   }
+ }"
+
+# data output from curl doesn't have a trailing newline
+echo
