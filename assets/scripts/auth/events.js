f82eadc7ce430d7ca449eda4d5f0dfd8af8d7236
tina513 <dlnaz.ai@hotmail.com>
2016-06-03 15:43:10 -0400

    worked on score board
    need to continue sign-up with sign-in on one button

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -14,8 +14,12 @@ const onSignUp = function (event) {
   event.preventDefault();
   let data = getFormFields(event.target);
   api.signUp(data)
-  .done(ui.success)
-  .fail(ui.failure);
+    .done(ui.successSignUp)
+    .fail(ui.failure);
+  // api.signIn(data.credentials.email, data.credentials.password)
+  //   .done(ui.signInSuccess)
+  //   .fail(ui.failure)
+ $('.game-board').unbind("click");
 };
 
 const onLogIn = function (event) {
@@ -24,13 +28,29 @@ const onLogIn = function (event) {
   api.logIn(data)
   .done(ui.signInSuccess)
   .fail(ui.failure);
+  $('.game-board').unbind("click");
 };
 
 const onSignOut = function (event) {
   event.preventDefault();
   api.signOut()
-  .done(ui.signOutSuccess)
-  .fail(ui.failure);
+    .done(ui.signOutSuccess)
+    .fail(ui.failure);
+  for (let i = 1; i < 10; i++) {
+    $('#column'+i).text("");
+  }
+  document.getElementById('Player_x-message').style.display = 'none';
+  document.getElementById('Player_o-message').style.display = 'none';
+  document.getElementById('tie-message').style.display = 'none';
+  $('.game-board').unbind("click");
+  document.getElementById('get-game').style.display = 'block';
+  document.getElementById('start-game').style.display = 'block';
+  for (let i = 1; i < 10; i++) {
+    $('#column'+i).text("");
+  }
+  $('#Player_x-score').val('');
+  $('#Player_o-score').val('');
+  $('#tie-score').val('');
 };
 
 const onChangePassword = function (event) {
@@ -39,13 +59,13 @@ const onChangePassword = function (event) {
   api.changePassword(data)
   .done(ui.success)
   .fail(ui.failure);
+  $('.game-board').unbind("click");
 };
 
 let arr = ['','','','','','','','',''];
-let Player_x_count = 0;
-let Player_o_count = 0;
-let tie_count = 0;
-
+let Player_x_count = $('#Player_x-score').val();
+let Player_o_count = $('#Player_o-score').val();
+let tie_count = $('#tie-score').val();
 const showAndSetMove = function (event) {
   event.preventDefault();
   let id = "#" + event.target.id;
@@ -55,30 +75,53 @@ const showAndSetMove = function (event) {
   }else{
     $(id).text(setMoveFunc);
     arr[id_num] = $(id).text();
-    let over = "false";
+    let over = false;
     if (checkWin(arr)==="Player_x win!") {
       document.getElementById('Player_x-message').style.display = 'block';
       $('.game-board').unbind("click");
       Player_x_count++;
-      $('#Player_x-score').val(Player_x_count);
-      over = "true";
+      over = true;
     }else if (checkWin(arr)==="Player_o win!") {
       document.getElementById('Player_o-message').style.display = 'block';
       $('.game-board').unbind("click");
       Player_o_count++;
-      $('#Player_o-score').val(Player_o_count);
-      over = "true";
+      over = true;
     }else if (arr.every(notEmpty)) {
       document.getElementById('tie-message').style.display = 'block';
       $('.game-board').unbind("click");
       tie_count++;
       $('#tie-score').val(tie_count);
-      over = "true";
+      over = true;
     }
+    $('#Player_x-score').val(Player_x_count);
+    $('#Player_o-score').val(Player_o_count);
+    $('#tie-score').val(tie_count);
+
     api.updateGameStatus(id_num, arr[id_num], over)
       .done(ui.success)
       .fail(ui.failure);
   }
+};
+
+const startNew = function (event) {
+  event.preventDefault();
+  $('#Player_x-score').val('');
+  $('#Player_o-score').val('');
+  $('#tie-score').val('');
+  api.createGame()
+    .done(ui.createGameSuccess)
+    .fail(ui.failure);
+  for (let i = 1; i < 10; i++) {
+    $('#column'+i).text("");
+   }
+  document.getElementById('Player_x-message').style.display = 'none';
+  document.getElementById('Player_o-message').style.display = 'none';
+  document.getElementById('tie-message').style.display = 'none';
+  $('.game-board').bind("click");
+  arr = ['','','','','','','','',''];
+  $('.game-board').on('click', showAndSetMove);
+  document.getElementById('start-game').style.display = 'none';
+  document.getElementById('get-game').style.display = 'none';
 };
 
 const clearBoard = function (event) {
@@ -99,11 +142,10 @@ const clearBoard = function (event) {
 
 const getGame = function (event) {
   event.preventDefault();
-  let id = $("#game-id").val();
-  api.getGameApi(id)
-  .done(ui.getGameSuccess)
-  .fail(ui.failure);
-  
+  //let id = $("#game-id").val();
+  api.getGameApi()
+    .done(ui.getGameSuccess)
+    .fail(ui.failure);
 };
 
 const addHandlers = () => {
@@ -115,6 +157,7 @@ const addHandlers = () => {
   $('#Player_o-score').val(Player_o_count);
   $('#tie-score').val(tie_count);
   $('.game-board').on('click', showAndSetMove);
+  $('#start-game').on('click', startNew);
   $('#play-game').on('click', clearBoard);
   $('#get-game').on('click', getGame);
 };
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,8 +1,13 @@
 'use strict';
 
 const app = require('./app.js');
+const small_func = require('./smfunction');
+const api = require('./api');
 
 let gameId = 0;
+let gameCell = [];
+let gameStatus = null;
+let checkWin = small_func.check;
 
 const success = (data) => {
   console.log(data);
@@ -11,6 +16,15 @@ const success = (data) => {
 const failure = (error) => {
   console.error(error);
 };
+
+//need to work on
+const successSignUp = function (data) {
+ //let email = $('#sign-in-email').val($('#sign-up-email').val());
+ //let ps = $('#sign-in-pw').val($('#sign-up-pw').val());
+ api.signIn(data.credentials.email, data.credentials.password)
+   .done(ui.signInSuccess)
+   .fail(ui.failure)
+}
 
 const signInSuccess = function (data) {
   app.user = data.user;
@@ -24,21 +38,45 @@ const signOutSuccess = function () {
 
 const createGameSuccess = function (data) {
   gameId = data.game.id;
-  console.log(data);
 };
 
 const getGameSuccess = function (data) {
-  if (data.games) {
+  //if (data.games) {
     console.table(data.games);
-  }else {
-    console.log(data.game);
-  }
+    let gameArr = data.games;
+    let x_score_count = 0;
+    let o_score_count = 0;
+    let tie_score_count = 0;
+    for(let i = 0; i < gameArr.length; i++) {
+      if ((checkWin(gameArr[i].cells)==="Player_x win!") && (gameArr[i].over===true)) {
+          x_score_count++;
+      }else if ((checkWin(gameArr[i].cells)==="Player_o win!") && (gameArr[i].over===true)) {
+          o_score_count++;
+      }else if (gameArr[i].over===true) {
+          tie_score_count++;
+      }
+    }
+    $('#Player_x-score').val(x_score_count);
+    $('#Player_o-score').val(o_score_count);
+    $('#tie-score').val(tie_score_count);
+//  }
+  // else {
+  //   console.log(data.game);
+  //   gameCell = data.game.cells;
+  //   gameStatus = data.game.over;
+  //   if ((checkWin(gameCell)==="Player_x win!") && (gameStatus===true)) {
+  //       $('#Player_x-score').val(1);
+  //   }else if ((checkWin(gameCell)==="Player_o win!") && (gameStatus===true)) {
+  //       $('#Player_o-score').val(1);
+  //   }else if (gameStatus===true) {
+  //       $('#tie-score').val(1);
+  //   }
+  // }
 };
 
 const returnGameId = function () {
   return gameId;
 };
-
 
 module.exports = {
   failure,
